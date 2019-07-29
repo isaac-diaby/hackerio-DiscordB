6587ac237a8bff613535834ee9779d30250157c4
Isaac Diaby <isaac.diaby.code@gmail.com>
2019-07-29 09:33:22 +0100

    Deploy v1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //import { config } from "dotenv"
-require('dotenv').config()
+// require('dotenv').config()
 //config()
 import { Database } from './Database';
 import { DiscordBotRun } from './discordBot';
